07631867551ae46f18664cf0a1d438540b76fdf2
nabbilaa <nistighfareta@gmail.com>
2025-01-10 09:39:08 +0700

    up

---
+++ PHP/CRUD - B/footer.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+    <footer class="blockquote-footer fixed-bottom">RPL B 2023
+    <cite title="Source Title"><a href="https://instagram.com/rplbclass"
+                target="_blank">RPL B 2023</a></cite></footer>
+</body>
+
+</html>
